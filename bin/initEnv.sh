dcfaee50037cd5c57e7015045fbc76e0886c71ad
sbilly <superli_1980@hotmail.com>
2014-02-06 12:41:37 +0800

    初始化脚本

---
+++ bin/initEnv.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# @Author: billy
+# @Date:   2014-02-06 12:35:33
+# @Last Modified by:   billy
+# @Last Modified time: 2014-02-06 12:39:38
+
+apt-get update
+apt-get install nload htop tmux curl lftp
+apt-get install build-essential
+apt-get intall python python-dev
+/bin/env/python ~/bin/get-pip.py
+pip install virtualenv virtualenvwrapper
